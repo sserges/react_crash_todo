75d4d04e7f54b8a0e49920fee76f2c94d470ac91
sserges <agounkeserge@gmail.com>
2019-03-07 14:28:36 +0000

    production code builded

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 
-import uuid from "uuid";
+// import uuid from "uuid";
+import axios from "axios";
 
 import Todos from "./components/Todos";
 import Header from "./components/layout/Header";
@@ -11,24 +12,14 @@ import About from "./components/pages/About";
 
 class App extends Component {
   state = {
-    todos: [
-      {
-        id: uuid.v4(),
-        title: "Take out the trash",
-        completed: false
-      },
-      {
-        id: uuid.v4(),
-        title: "Dinner with wife",
-        completed: false
-      },
-      {
-        id: uuid.v4(),
-        title: "Meeting with boss",
-        completed: false
-      }
-    ]
+    todos: []
   };
+
+  componentDidMount() {
+    axios
+      .get("https://jsonplaceholder.typicode.com/todos?_limit=10")
+      .then(res => this.setState({ todos: res.data }));
+  }
 
   // Toggle Complete
   markComplete = id => {
@@ -43,19 +34,21 @@ class App extends Component {
   };
 
   delTodo = id => {
-    this.setState({
-      todos: [...this.state.todos.filter(todo => todo.id !== id)]
-    });
+    axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`).then(res =>
+      this.setState({
+        todos: [...this.state.todos.filter(todo => todo.id !== id)]
+      })
+    );
   };
 
   // Add Todo
   addTodo = title => {
-    const newTodo = {
-      id: uuid.v4(),
-      title,
-      completed: false
-    };
-    this.setState({ todos: [...this.state.todos, newTodo] });
+    axios
+      .post("https://jsonplaceholder.typicode.com/todos", {
+        title,
+        completed: false
+      })
+      .then(res => this.setState({ todos: [...this.state.todos, res.data] }));
   };
 
   render() {
--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 export class AddTodo extends Component {
   state = {
@@ -34,5 +35,9 @@ export class AddTodo extends Component {
     );
   }
 }
+
+AddTodo.propTypes = {
+  addTodo: PropTypes.func.isRequired
+};
 
 export default AddTodo;
